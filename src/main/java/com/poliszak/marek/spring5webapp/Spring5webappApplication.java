eb469187bfe8e1205ffbb54ce4240a14e4cc9155
Marek <marekpoliszak@gmail.com>
2022-10-03 17:28:34 +0200

    first

---
+++ src/main/java/com/poliszak/marek/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,13 @@
+package com.poliszak.marek.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+
+}
